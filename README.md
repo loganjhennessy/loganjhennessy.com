f07c404e1ed3173860f29dc8d90e10cc9a8188b0
Logan Hennessy <loganjhennessy@gmail.com>
2019-05-23 22:01:38 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # loganjhennessy.com
 
 Personal website.
+
+## Deployment steps
+
+```
+docker build -t gcr.io/logan-hennessy/loganjhennessy.com .
+docker push gcr.io/logan-hennessy/loganjhennessy.com
+sudo gcloud beta run deploy loganjhennessy.com --image gcr.io/logan-hennessy/loganjhennessy.com
+```
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("public")))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
